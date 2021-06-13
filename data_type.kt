416edba266b0c44106a6ee43292f03d5b38659ef
Muhammad Fauzan Hadi Saputra <fraternityfra@gmail.com>
2021-06-13 11:14:26 +0700

    third commit

---
+++ data_type.kt
@@ -0,0 +1,16 @@
+//belajar tipe data
+
+fun main(args: Array<String>) {
+//type data pada kotlin
+var language: String = "French"
+val score: Int = 95
+
+//cetak
+println(language);
+println(score);
+}
+
+/*perbedaan val dan var
+  variabel yang dideklarasikan dengan "val" tidak dapat diubah nilai nya, mirip dengan final di java
+  variabl yang dideklarasikan dengan "var" dapat diubah nilai nya
+ */

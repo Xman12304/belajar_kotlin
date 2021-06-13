155a14b6ec634172462c6c8f146215767cd4c765
Muhammad Fauzan Hadi Saputra <fraternityfra@gmail.com>
2021-06-13 17:50:35 +0700

    revisi kedua untuk data type

--- data_type.kt
+++ data_type.kt
@@ -3,13 +3,21 @@
 fun main(args: Array<String>) {
 
 //type data pada kotlin
-var language: String = "French"
-val score: Int = 95
-
+var language: String = "French"; //tipe data string
+val score: Int = 95; //tipe data integer
+var tujuan: Boolean = true; //tipe data boolean
+var huruf: Char = 'K' //tipe data char
+var angka: Number = 120; //tipe data number
+var angka1: Double = 999.5; //tipe data double = double precision 64 bit
+var angka2: Float = 19.5F; //tipe data float = single precision 32 bit, diberikan "F" diakhir sebagai penanda kalo ini tipe float
 //cetak
 println(language);
 println(score);
-
+println(tujuan);
+println(huruf);
+println(angka);
+println(angka1);
+println(angka2);
 
  //contoh perbedaan "var" dan "val"
 

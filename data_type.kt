13f6ba3e3b9124f80742ae08bc31741cdcfc150c
Muhammad Fauzan Hadi Saputra <fraternityfra@gmail.com>
2021-06-13 17:12:14 +0700

    revisi untuk data type

--- data_type.kt
+++ data_type.kt
@@ -1,6 +1,7 @@
 //belajar tipe data
 
 fun main(args: Array<String>) {
+
 //type data pada kotlin
 var language: String = "French"
 val score: Int = 95
@@ -8,9 +9,23 @@ val score: Int = 95
 //cetak
 println(language);
 println(score);
-}
 
-/*perbedaan val dan var
-  variabel yang dideklarasikan dengan "val" tidak dapat diubah nilai nya, mirip dengan final di java
-  variabl yang dideklarasikan dengan "var" dapat diubah nilai nya
+
+ //contoh perbedaan "var" dan "val"
+
+ /*
+ jika menggunakan "var" maka nilai dapat diubah
  */
+ var bahasa: String = "Indonesia"
+ bahasa = "jawa"
+ println(bahasa);
+
+/*
+jika menggunakan "val" maka nilai tidak dapat diubah, pada contoh dibawah ini apabila kita cetak
+maka akan menghasilkan pesan error "val cannot be reassigned"
+*/
+val nilai: Int = 35
+nilai = 15
+println(nilai);
+
+}

75fdfd294532b5d6951309ae0ea5bd9fe4db7dfc
Muhammad Fauzan Hadi Saputra <fraternityfra@gmail.com>
2021-06-19 06:36:10 +0700

    belajar operator

---
+++ operator.kt
@@ -0,0 +1,21 @@
+fun main(args: Array<String>) {
+    val number1 = 12;
+    val number2 = 5;
+    var result: Int
+
+    result = number1 + number2
+    println("$number1 + $number2 = $result");
+
+    result = number1 - number2
+    println("$number1 - $number2 = $result");
+
+    result = number1 * number2
+    println("$number1 * $number2 = $result");
+
+    result = number1 / number2
+    println("$number1 / $number2 = $result");
+
+    result = number1 % number2
+    println("$number1 % $number2 = $result");
+
+}
